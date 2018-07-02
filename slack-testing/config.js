8e556fc61dc3033ce688ff625070751db48c6ec8
Jayakrishna Sahit <jayakrishna.sahit@iitgn.ac.in>
2018-07-02 16:25:18 +0530

    Update config.js

--- slack-testing/config.js
+++ slack-testing/config.js
@@ -4,5 +4,5 @@ module.exports = {
 	clientSecret : '3fa0407a98ed0f88b96263db05c395f8',
 	verificationToken : '9iNrPkl7Pb7wLl7h28xtxpbF', 
 	signingSecret : '2f2653e6546e4e633f9c605e13e3860c',
-	token:'xoxp-381900205446-382057290071-391413233554-1844127e8b8ccc51cbd57ac86417c0c1'
+	token: *insert slack token here*
 }
